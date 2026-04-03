60dd395e1755ae2cc078257641048159dad38386
vmgabruk-boop <v.m.gabruk@gmail.com>
2026-04-03 11:54:41 +0300

    Add files via upload

---
+++ Запуск проекта_Установка зависимостей.sh
@@ -0,0 +1,15 @@
+# Создайте папку проекта
+mkdir truck_counter
+cd truck_counter
+
+# Создайте виртуальное окружение (рекомендуется)
+python -m venv venv
+
+# Активируйте его:
+# Windows:
+venv\Scripts\activate
+# Mac/Linux:
+source venv/bin/activate
+
+# Установите зависимости
+pip install -r requirements.txt
